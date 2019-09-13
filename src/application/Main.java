a3c51db7a2b99815e05f7b14e146c093c83a03c9
GabrielaAllende <gabrielaallende15@hotmail.com>
2019-09-13 18:30:36 -0400

    Nuevos cambios

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,23 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Proyecto.fxml"));
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader fXMLLoader = new FXMLLoader(getClass().getResource("/view/TituloFX"));
+			AnchorPane root = fXMLLoader.load();
+			//ScreenFX screenFX = fXMLLoader.getController();
+			Scene scene = new Scene(root,root.getPrefWidth(),root.getPrefHeight());
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.getIcons().add (new Image ("/image/pizzaIcon.png"));
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
