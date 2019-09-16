69f110c805e249e17092aa250dc604dafa267f76
AndySarmimo <anbesamo@gmail.com>
2019-09-16 18:04:03 -0400

    haciendo la modificacion a venta

---
+++ src/model/Mesa.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Mesa {
+	private int codigoMesa;
+	private String estado;
+	private int capacidad;
+	
+	public Mesa (int codigoMesa, String estado, int capacidad) {
+		this.codigoMesa = codigoMesa;
+		this.estado = estado ;
+	
+		this.capacidad = capacidad;
+		
+	}
+	
+}
